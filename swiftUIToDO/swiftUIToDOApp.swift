0293a7830b34afd1ac2a046579eb62c99918a56b
juma <juma.durduyev@gmail.com>
2021-09-21 07:58:56 -0700

    Initial Commit

---
+++ swiftUIToDO/swiftUIToDOApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUIToDOApp.swift
+//  swiftUIToDO
+//
+//  Created by Juma on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct swiftUIToDOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
