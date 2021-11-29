3e61131123a923b866ff48849bb06ee675e85e7a
juma <juma.durduyev@gmail.com>
2021-11-29 10:43:55 -0800

    adding some text

--- swiftUIToDO/ContentView.swift
+++ swiftUIToDO/ContentView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("First of all")
+                .padding()
+            Text("Hello, world!")
+                .padding()
+        }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
